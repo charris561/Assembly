9d34f12f80cab92e94b051e36c357314c4e678dc
charris561 <89329141+charris561@users.noreply.github.com>
2023-02-13 01:09:14 -0700

    Update bits.c

--- labs/datalab-handout/bits.c
+++ labs/datalab-handout/bits.c
@@ -247,7 +247,7 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  return ((!x << 31 >> 31) & (z ^ y)) ^ y;
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
